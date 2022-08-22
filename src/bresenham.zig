924648f714a06968daac42a7615d0ebc45f6b83b
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-08-22 23:12:04 +0100

    delete unused code

--- src/bresenham.zig
+++ src/bresenham.zig
@@ -3,8 +3,6 @@ const std = @import("std");
 /// A piece-wise implementaion of Bresenham's line algorithm (https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm)
 /// `plotter` should be something defining a `plot(x, y) bool` member function that returns false if plotting should stop
 pub fn line(x0: i32, y0: i32, x1: i32, y1: i32, plotter: anytype) void {
-    std.debug.assert(@typeInfo(@TypeOf(plotter)) == .Pointer);
-
     var x = x0;
     var y = y0;
     var dx = abs(x1 - x0);
@@ -32,58 +30,6 @@ pub fn line(x0: i32, y0: i32, x1: i32, y1: i32, plotter: anytype) void {
             if (y == y1) break;
             err = err + dx;
             y = y + sy;
-        }
-    }
-}
-
-fn line_low(x0: i32, y0: i32, x1: i32, y1: i32, plotter: anytype) void {
-    var dx = x1 - x0;
-    var dy = y1 - y0;
-    var yi: i32 = 1;
-
-    if (dy < 0) {
-        yi = -1;
-        dy = -dy;
-    }
-
-    var d = (2 * dy) - dx;
-    var y = y0;
-    var x = x0;
-    while (x <= x1) : (x += 1) {
-        if (plotter.plot(x, y) == false) {
-            return;
-        }
-        if (d > 0) {
-            y = y + yi;
-            d = d + (2 * (dy - dx));
-        } else {
-            d = d + 2 * dy;
-        }
-    }
-}
-
-fn line_high(x0: i32, y0: i32, x1: i32, y1: i32, plotter: anytype) void {
-    var dx = x1 - x0;
-    var dy = y1 - y0;
-    var xi: i32 = 1;
-
-    if (dx < 0) {
-        xi = -1;
-        dx = -dx;
-    }
-
-    var d = (2 * dx) - dy;
-    var x = x0;
-    var y = y0;
-    while (y <= y1) : (y += 1) {
-        if (plotter.plot(x, y) == false) {
-            return;
-        }
-        if (d > 0) {
-            x = x + xi;
-            d = d + (2 * (dx - dy));
-        } else {
-            d = d + 2 * dx;
         }
     }
 }
