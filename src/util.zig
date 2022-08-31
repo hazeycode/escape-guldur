86ec30c42af3fb482583d12a288fdfb7c5a3d48b
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-09-01 00:27:09 +0100

    fix quicksort

--- src/util.zig
+++ src/util.zig
@@ -8,34 +8,34 @@ pub fn swap(values: anytype, i: usize, j: usize) void {
 
 pub fn quicksort(
     values: anytype,
-    low: usize,
-    high: usize,
+    low: isize,
+    high: isize,
     comparitor: anytype,
 ) void {
-    if (low < high) {
-        const p = partition(values, low, high, comparitor);
-        quicksort(values, low, p - 1, comparitor);
-        quicksort(values, p + 1, high, comparitor);
-    }
+    if (low >= high) return;
+    const p = partition(values, low, high, comparitor);
+    quicksort(values, low, p - 1, comparitor);
+    quicksort(values, p + 1, high, comparitor);
 }
 
 fn partition(
     values: anytype,
-    low: usize,
-    high: usize,
+    low: isize,
+    high: isize,
     comparitor: anytype,
-) usize {
-    const pivot = values[high];
-    var i = @intCast(isize, low) - 1;
-    var j = low;
-    while (j <= high - 1) : (j += 1) {
+) isize {
+    const pivot = values[@intCast(usize, high)];
+    var i = low - 1;
+    var j = @intCast(usize, low);
+    while (j < high) : (j += 1) {
         if (comparitor.compare(values[j], pivot)) {
             i += 1;
             swap(values, @intCast(usize, i), j);
         }
     }
-    swap(values, @intCast(usize, i + 1), high);
-    return @intCast(usize, i + 1);
+    i += 1;
+    swap(values, @intCast(usize, i), @intCast(usize, high));
+    return i;
 }
 
 const testing = std.testing;
@@ -47,10 +47,20 @@ test "quicksort" {
         }
     }{};
 
-    var values = [_]u32{ 3, 7, 4, 234, 4, 19, 19 };
-    quicksort(&values, 0, values.len - 1, comparitor);
+    {
+        var values = [_]u32{ 3, 7, 4, 234, 4, 19, 19 };
+        quicksort(&values, 0, values.len - 1, comparitor);
 
-    const expected = [_]u32{ 3, 4, 4, 7, 19, 19, 234 };
+        const expected = [_]u32{ 3, 4, 4, 7, 19, 19, 234 };
 
-    try testing.expectEqualSlices(u32, &expected, &values);
+        try testing.expectEqualSlices(u32, &expected, &values);
+    }
+    {
+        var values = [_]u32{ 3, 3 };
+        quicksort(&values, 0, values.len - 1, comparitor);
+
+        const expected = [_]u32{ 3, 3 };
+
+        try testing.expectEqualSlices(u32, &expected, &values);
+    }
 }
