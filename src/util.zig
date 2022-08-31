ea5caddf3da3025376940b2faa4e194a585b6a28
Chris Heyes <22148308+hazeycode@users.noreply.github.com>
2022-08-31 20:36:39 +0100

    generalise quicksort

--- src/util.zig
+++ src/util.zig
@@ -6,20 +6,30 @@ pub fn swap(values: anytype, i: usize, j: usize) void {
     values[j] = temp;
 }
 
-pub fn quicksort(values: anytype, low: usize, high: usize) void {
+pub fn quicksort(
+    values: anytype,
+    low: usize,
+    high: usize,
+    comparitor: anytype,
+) void {
     if (low < high) {
-        const p = partition(values, low, high);
-        quicksort(values, low, p - 1);
-        quicksort(values, p + 1, high);
+        const p = partition(values, low, high, comparitor);
+        quicksort(values, low, p - 1, comparitor);
+        quicksort(values, p + 1, high, comparitor);
     }
 }
 
-fn partition(values: anytype, low: usize, high: usize) usize {
+fn partition(
+    values: anytype,
+    low: usize,
+    high: usize,
+    comparitor: anytype,
+) usize {
     const pivot = values[high];
     var i = @intCast(isize, low) - 1;
     var j = low;
     while (j <= high - 1) : (j += 1) {
-        if (values[j] < pivot) {
+        if (comparitor.compare(values[j], pivot)) {
             i += 1;
             swap(values, @intCast(usize, i), j);
         }
@@ -31,8 +41,14 @@ fn partition(values: anytype, low: usize, high: usize) usize {
 const testing = std.testing;
 
 test "quicksort" {
+    var comparitor = struct {
+        pub fn compare(_: @This(), a: u32, b: u32) bool {
+            return a < b;
+        }
+    }{};
+
     var values = [_]u32{ 3, 7, 4, 234, 4, 19, 19 };
-    quicksort(&values, 0, values.len - 1);
+    quicksort(&values, 0, values.len - 1, comparitor);
 
     const expected = [_]u32{ 3, 4, 4, 7, 19, 19, 234 };
 
